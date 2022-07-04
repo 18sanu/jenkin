59e1233e92b5c2838cd2b940b3738aa06dac29e8
shivaay-singh <shiven1503@gmail.com>
2022-07-04 16:03:24 +0530

    shiv first commit

--- SecondProgram.java
+++ SecondProgram.java
@@ -2,5 +2,8 @@ public class SecondProgram {
 	public static void main(String [] args){
 		System.out.println("hello world2");
 	}
+	public static void shiv(){
+		System.out.println("from shiv world2");
+	}
 	
 }
